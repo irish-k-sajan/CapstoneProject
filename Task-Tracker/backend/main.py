e087610c6c197b09ae6ac98549709c0e8c82c6de
irish-k-sajan <irishk.sajan@tigeranalytics.com>
2024-11-26 14:32:41 +0530

    Changes in backend

--- Task-Tracker/backend/main.py
+++ Task-Tracker/backend/main.py
@@ -7,6 +7,7 @@ from typing import  Annotated, Optional
 from database import engine,SessionLocal
 from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
+from uuid import UUID
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -29,37 +30,49 @@ app.add_middleware(
 )
 
 class ProjectBase(BaseModel):
-    #project_id: int
     project_name:str
     project_description: str
     start_date: date
     end_date: date
-    project_owner_id:int
+    project_owner_id:str
 class UpdateProjectBase(BaseModel):
     project_name: Optional[str]=None
     project_description: Optional[str]=None
     start_date: Optional[date]=None
     end_date: Optional[date]=None
-    project_owner_id:Optional[int]=None
+    project_owner_id:Optional[str]=None
 
 class TaskBase(BaseModel):
-    #task_id :int
     task_name: str
     task_description:str
     task_status :str
-    task_owner_id: int
+    task_owner_id: str
     due_date: date
-    project_id: int
+    project_id: UUID
 
 class UpdateTaskBase(BaseModel):
-    task_id :Optional[int]=None
     task_name: Optional[str]=None
     task_description:Optional[str]=None
     task_status :Optional[str]=None
-    task_owner_id: Optional[int]=None
+    task_owner_id: Optional[str]=None
     due_date: Optional[date]=None
-    project_id: Optional[int]=None
-
+    project_id: Optional[UUID]=None
+class EmployeeBase(BaseModel):
+    employee_id : str
+    employee_name :str
+    employee_email : str
+class RoleBase(BaseModel):
+    role_id:int
+    role_name:str
+class UserRoleBase(BaseModel):
+    role_id:int
+    project_id:UUID
+    employee_id:str
+class UpdateUserRoleBase(BaseModel):
+    role_id:Optional[int]=None
+    project_id:Optional[UUID]=None
+    employee_id:Optional[str]=None
+    
 def get_db():
     db=SessionLocal()
     try:
@@ -77,28 +90,42 @@ async def create_task(task: TaskBase,db:db_dependency):
     db_task=models.Task(**task.dict())
     db.add(db_task)
     db.commit()
+@app.get("/user",status_code=status.HTTP_200_OK)
+async def get_user(db:db_dependency):
+    users=db.query(models.Employee).all()
+    if users is None:
+        raise HTTPException(status_code=404,detail="No projects")
+    return users
+    
+
+@app.post("/create-user/",status_code=status.HTTP_201_CREATED)
+async def create_users(emp:EmployeeBase,db: db_dependency):
+    db_emp=models.Project(**emp.dict())
+    db.add(db_emp)
+    db.commit()
+
 
 @app.get('/projects/{project_id}',status_code=status.HTTP_200_OK)
-async def read_project(project_id:int, db:db_dependency):
+async def read_project(project_id:UUID, db:db_dependency):
     project=db.query(models.Project).filter(models.Project.project_id==project_id).first()
     if project is None:
         raise HTTPException(status_code=404,detail='Project not found')
     return project
 @app.get('/tasks/{task_id}',status_code=status.HTTP_200_OK)
-async def read_task(task_id: int, db:db_dependency):
+async def read_task(task_id: UUID, db:db_dependency):
     task=db.query(models.Task).filter(models.Task.task_id==task_id).first()
     if task is None:
         raise HTTPException(status_code=404,detail="Task not found")
     return task
 @app.delete('/tasks/{task_id}',status_code=status.HTTP_200_OK)
-async def delete_task(task_id: int, db:db_dependency):
+async def delete_task(task_id: UUID, db:db_dependency):
     task=db.query(models.Task).filter(models.Task.task_id==task_id).first()
     if task is None:
         raise HTTPException(status_code=404,detail="Task not found")
     db.delete(task)
     db.commit()
 @app.delete('/projects/{project_id}',status_code=status.HTTP_200_OK)
-async def delete_project(project_id: int, db:db_dependency):
+async def delete_project(project_id: UUID, db:db_dependency):
     project=db.query(models.Project).filter(models.Project.project_id==project_id).first()
     if project is None:
         raise HTTPException(status_code=404,detail="Project not found")
@@ -117,14 +144,43 @@ async def get_tasks(db:db_dependency):
         raise HTTPException(status_code=404,detail="No projects")
     return tasks
 @app.put('/update-project/{project_id}',status_code=status.HTTP_202_ACCEPTED)
-async def update_project(project_id: int,project: UpdateProjectBase,db: db_dependency):
+async def update_project(project_id: UUID,project: UpdateProjectBase,db: db_dependency):
     db_project=db.query(models.Project).filter(models.Project.project_id==project_id).first()
     for key, value in project.model_dump(exclude_unset=True).items():
         setattr(db_project, key, value)
     db.commit()
 @app.put('/update-task/{task_id}',status_code=status.HTTP_202_ACCEPTED)
-async def update_task(task_id: int,task: UpdateTaskBase,db: db_dependency):
+async def update_task(task_id: UUID,task: UpdateTaskBase,db: db_dependency):
     db_task=db.query(models.Task).filter(models.Task.task_id==task_id).first()
     for key, value in task.model_dump(exclude_unset=True).items():
         setattr(db_task, key, value)
     db.commit()
+@app.post('/create-role',status_code=status.HTTP_201_CREATED)
+async def create_role(role:RoleBase,db:db_dependency):
+    db_role=models.Role(**role.dict())
+    db.add(db_role)
+    db.commit()
+@app.post('/create-user-role',status_code=status.HTTP_201_CREATED)
+async def create_user_role(user_role:UserRoleBase,db:db_dependency):
+    db_role=models.Role(**user_role.dict())
+    db.add(db_role)
+    db.commit()
+@app.put('/update-user-role/{user_role_id}',status_code=status.HTTP_202_ACCEPTED)
+async def update_user_role(user_role_id: int,user_role: UpdateUserRoleBase,db:db_dependency):
+    db_user_role=db.query(models.UserRole).filter(models.UserRole.user_role_id==user_role_id).first()
+    for key, value in user_role.model_dump(exclude_unset=True).items():
+        setattr(db_user_role, key, value)
+    db.commit()
+@app.delete('/delete-user-role/{user_role_id}', status_code=status.HTTP_200_OK)
+async def delete_user_role(user_role_id: int, db:db_dependency):
+    db_user_role=db.query(models.UserRole).filter(models.UserRole.user_role_id==user_role_id).first()
+    if db_user_role is None:
+        raise HTTPException(status_code=404,detail="Project not found")
+    db.delete(db_user_role)
+    db.commit()
+
+
+    
+
+
+
