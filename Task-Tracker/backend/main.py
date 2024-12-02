f479aae2d0384557609eab2f135ced8dcba4f97d
irish-k-sajan <irishk.sajan@tigeranalytics.com>
2024-12-02 13:10:44 +0530

    backend bug fixes

--- Task-Tracker/backend/main.py
+++ Task-Tracker/backend/main.py
@@ -435,7 +435,8 @@ def get_read_only_users_tasks(project_id:str,task_id:str,user_id: str,db:db_depe
         db_users_id=db.query(models.TaskUserRole).filter(models.TaskUserRole.task_id==task_id).all()
         user_details=[]
         for user in db_users_id:
-            user_details.append(db.query(models.Employee).filter(models.Employee.employee_id==user.employee_id)).first()
+            print(user.employee_id)
+            user_details.append(db.query(models.Employee).filter(models.Employee.employee_id==user.employee_id).first())
         return user_details
     else:
         raise HTTPException(status_code=403, detail="Access Denied")
