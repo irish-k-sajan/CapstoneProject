e2d1a5ee7c7a69b7d3987d96e737a27847ffbe16
irish-k-sajan <irishk.sajan@tigeranalytics.com>
2024-12-02 10:45:06 +0530

    UI designing

--- Task-Tracker/frontend/src/components/Logout.jsx
+++ Task-Tracker/frontend/src/components/Logout.jsx
@@ -17,7 +17,7 @@ export default function Logout(){
             onClick={renderProps.onClick}
             className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600 transition duration-300"
         >
-            
+            Logout
         </button>
     )}
     buttonText=''
